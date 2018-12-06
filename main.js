4b0e4270014675152827e138b34e49d6577c09d9
sravan.reddy <sravan.reddy@lithium.com>
2018-12-06 11:00:43 +0530

    Commit newcreated on boarding ui

---
+++ main.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var fs = require("fs");
+
+http.createServer(function(request, response) {
+
+    
+    // if(request.url === "/index.html"){
+        console.log(request.url);
+
+        fs.readFile(request.url.slice(1), function(err, data){
+            if(err){
+
+               response.writeHead(404);
+               response.write("Not Found!");
+            }
+            else{
+               response.writeHead(200);
+               response.write(data);
+            }
+            response.end();
+         });
+    //  }
+    //  else{
+    //     response.writeHead(200, {'Content-Type': 'text/html'});
+    //     response.write('<b>Hey there!</b><br /><br />This is the default response. Requested URL is: ' + request.url);
+    //     response.end();
+    //  }
+     
+
+}).listen(3000);
+
